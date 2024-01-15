7c9ae69ef4a125d05465ebe55624c317a30f1c44
snadi043 <snadi043@uottawa.ca>
2024-01-15 18:46:25 -0500

    Initial versions of further custom widgets to implement the Questionnaire feature in the Quiz app.

---
+++ quiz_app/lib/quiz.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/gradient_main.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: GradientMain(),
+      ),
+    );
+  }
+}
